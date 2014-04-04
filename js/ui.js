2d0796c5f7d9eb0f7dd6ac69a9c1c3c59ddc00ee
jackie19 <jackie198512@126.com>
2014-04-04 17:48:01 +0800

    update1

--- js/ui.js
+++ js/ui.js
@@ -41,6 +41,103 @@
         return __super;
     })();
 
+
+// 更新：
+// 05.27: 1、保证回调执行顺序：error > ready > load；2、回调函数this指向img本身
+// 04-02: 1、增加图片完全加载后的回调 2、提高性能
+    /**
+     * 图片头数据加载就绪事件 - 更快获取图片尺寸
+     * @version  2011.05.27
+     * @author  TangBin
+     * @see    http://www.planeart.cn/?p=1121
+     * @param  {String}  图片路径
+     * @param  {Function}  尺寸就绪
+     * @param  {Function}  加载完毕 (可选)
+     * @param  {Function}  加载错误 (可选)
+     * @example imgReady('http://www.google.com.hk/intl/zh-CN/images/logo_cn.png', function () {
+    alert('size ready: width=' + this.width + '; height=' + this.height);
+  });
+     */
+    var imgReady = (function () {
+        var list = [], intervalId = null,
+
+        // 用来执行队列
+            tick = function () {
+                var i = 0;
+                for (; i < list.length; i++) {
+                    list[i].end ? list.splice(i--, 1) : list[i]();
+                }
+                ;
+                !list.length && stop();
+            },
+
+        // 停止所有定时器队列
+            stop = function () {
+                clearInterval(intervalId);
+                intervalId = null;
+            };
+
+        return function (url, ready, load, error) {
+            var onready, width, height, newWidth, newHeight,
+                img = new Image();
+
+            img.src = url;
+
+            // 如果图片被缓存，则直接返回缓存数据
+            if (img.complete) {
+                ready.call(img);
+                load && load.call(img);
+                return;
+            }
+            ;
+
+            width = img.width;
+            height = img.height;
+
+            // 加载错误后的事件
+            img.onerror = function () {
+                error && error.call(img);
+                onready.end = true;
+                img = img.onload = img.onerror = null;
+            };
+
+            // 图片尺寸就绪
+            onready = function () {
+                newWidth = img.width;
+                newHeight = img.height;
+                if (newWidth !== width || newHeight !== height ||
+                    // 如果图片已经在其他地方加载可使用面积检测
+                    newWidth * newHeight > 1024
+                    ) {
+                    ready.call(img);
+                    onready.end = true;
+                }
+                ;
+            };
+            onready();
+
+            // 完全加载完毕的事件
+            img.onload = function () {
+                // onload在定时器时间差范围内可能比onready快
+                // 这里进行检查并保证onready优先执行
+                !onready.end && onready();
+
+                load && load.call(img);
+
+                // IE gif动画会循环执行onload，置空onload即可
+                img = img.onload = img.onerror = null;
+            };
+
+            // 加入队列中定期执行
+            if (!onready.end) {
+                list.push(onready);
+                // 无论何时只允许出现一个定时器，减少浏览器性能损耗
+                if (intervalId === null) intervalId = setInterval(tick, 40);
+            }
+            ;
+        };
+    })();
+
     var iRoom;
 
     iRoom = (function (_super) {
@@ -55,13 +152,47 @@
                 this.listItemTpl = '<li style="z-index:{index}" type="{type}"><img src="{thumb}" index="{index}" id="{id}" moudel="{moudel}" /></li>';
                 this.mask = $('.mask');
                 this.loading = $('.loading');
+                this.model = $('.model img');
                 this.container = $('.clothes');
                 this.types = $('li', '.types');
                 this.clothesListBox = $('.clothes-list-box').addClass('out slideup');
                 this.clothesList = $('.clothes-list');
-                this.dressing = [];
-                this.type = null;
+                this.dressing = []; //正在穿的 id
+                this.type = null; //服装类型：外套，衬衫...
+
+                this.sizeType = null; //屏幕与图片尺寸比例
+
                 this.bind();
+
+                this.setSize();
+            },
+            setSize   : function (elem) {
+                var screen_p,
+                    model,
+                    self = this,
+                    css = {};
+
+                if (!this.sizeType) {
+                    screen_p = window.outerWidth / window.outerHeight; // 720/1102
+                    model = this.model[0].src;
+                    self = this;
+
+                    imgReady(model, function () {
+                        var img_p = this.width / this.height;
+                        if (img_p > screen_p) {
+                            self.sizeType = 'width';
+                        } else {
+                            self.sizeType = 'height';
+                        }
+                        css[self.sizeType] = '100%';
+                        css.visibility = 'visible';
+                        self.model.css(css);
+                    });
+                } else {
+                    css[self.sizeType] = '100%';
+                    elem.css(css);
+                }
+
             },
             bind      : function () {
                 var self = this;
@@ -129,14 +260,17 @@
                 });
             },
             putOn     : function (data) {
-                var img = sub(this.listItemTpl, data);
+                var img = $(sub(this.listItemTpl, data)).hide();
+
+                this.setSize($('img', img));
+
 
                 if ($.inArray(data.id, this.dressing) == -1) {
                     this.undress(data);
                     this.dressing.push(data.id);
-                    this.container.append(img);
+                    this.container.append(img.fadeIn('fast'));
                 } else {
-
+                    this.undress(data);
                 }
             },
 //            todo 供翻页使用，未集成
@@ -158,7 +292,6 @@
                     });
 
             },
-            //todo 找到已经穿的
             renderList: function (data) {
                 var self = this;
                 var innerHTML = '';
@@ -170,13 +303,11 @@
                     self.clothesList.html(innerHTML);
                     self.dressing.forEach(function (id) {
 
-                        $.each($('img', self.clothesList), function (idx, img) {
+                        $.each($('img', self.clothesList), function () {
                             if (this.id == id) {
                                 $(this).addClass('selected');
                             }
                         });
-
-
                     });
                     self.clothesListBox.removeClass('out').addClass('in');
                 }
@@ -188,11 +319,19 @@
                 this[key].fadeOut('fast');
             },
             undress   : function (data) {
-                var self = this;
-//                todo 点击按钮脱单件
+                var self = this,
+                    li_zindex,
+                    li_type;
+//                todo 按类型脱单件
                 $('li', this.container).each(function (i, li) {
-//                    去除同类型，同id的
-                    if ($(li).css('z-index') == data.index && $(li).attr('type') == data.type) {
+                    li_zindex = $(li).css('z-index');
+                    li_type = $(li).attr('type');
+//                    去除同类型，同层级的
+                    if (
+                        (li_zindex == data.index && li_type == data.type)
+                            ||
+                            (typeof data.index == 'undefined')
+                        ) {
                         var id = $('img', li).attr('id');
                         var indexOf = self.dressing.indexOf(id);
                         self.dressing.splice(indexOf, 1);
@@ -207,7 +346,7 @@
             clear     : function () {
                 this.dressing = [];
                 this.container.html('');
-                return 'is clean.';
+                return 'true';
             }
 
         });
